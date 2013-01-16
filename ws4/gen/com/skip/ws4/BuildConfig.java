a0bff32027db4c1f7463ae2d76b77616183271ed
4UN <qwlho@hotmail.com>
2013-01-16 18:23:43 +0800

    WS

---
+++ ws4/gen/com/skip/ws4/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.skip.ws4;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ws4/src/com/skip/ws4/black.java
@@ -0,0 +1,51 @@
+package com.skip.ws4;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class black extends Activity {
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_black);	
+	        findviews();
+            setListensers();       
+	    }
+	 //private ImageView fzero;
+	 private ImageView mf;
+	 //private ImageView gc;
+	 private ImageView eva;
+	 //private ImageView ft;
+	 //private ImageView persona;
+	 private void findviews(){
+	     	mf=(ImageView)findViewById(R.id.mf);
+	     	 eva=(ImageView)findViewById(R.id.eva);
+	      }	
+	 private void setListensers(){
+			mf.setOnClickListener(mf_onclick);
+			eva.setOnClickListener(eva_onclick);
+		}
+		private OnClickListener mf_onclick = new OnClickListener(){
+	    	public void onClick(View view)
+	    	{
+	    		Intent mf_intent = new Intent();
+	    			mf_intent.setClass(black.this, Activity_list.class);
+	    		startActivity(mf_intent);
+	    		
+	    	}	
+	    };
+	    private OnClickListener eva_onclick = new OnClickListener(){
+	    	public void onClick(View view)
+	    	{
+	    		Intent eva_intent = new Intent();
+	    			eva_intent.setClass(black.this, Activity_list.class);
+	    		startActivity(eva_intent);
+	    		
+	    	}	
+	    }; 
+}
