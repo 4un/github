002c7811211f67b74624d9e92c1b45089f3f556f
4UN <qwlho@hotmail.com>
2013-05-04 18:25:35 +0800

    lol

--- HKACGFPM/gen/com/skip/hkacgfpm/R.java
+++ HKACGFPM/gen/com/skip/hkacgfpm/R.java
@@ -15,9 +15,10 @@ public final class R {
     }
     public static final class id {
         public static final int acc=0x7f070000;
-        public static final int menu_settings=0x7f070003;
+        public static final int menu_settings=0x7f070004;
         public static final int ok=0x7f070002;
         public static final int pw=0x7f070001;
+        public static final int result=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- HKACGFPM/src/com/skip/hkacgfpm/MainActivity.java
+++ HKACGFPM/src/com/skip/hkacgfpm/MainActivity.java
@@ -1,24 +1,37 @@
 package com.skip.hkacgfpm;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
     Button login;
 	EditText pass,acc;
-	String strpass,stracc;
+	TextView result;
+	String strpass,stracc,data;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,90 +39,64 @@ public class MainActivity extends Activity {
 		acc=(EditText)findViewById(R.id.acc);
 		pass=(EditText)findViewById(R.id.pw);
 		login=(Button)findViewById(R.id.ok);
-		login.setOnClickListener(new Button.OnClickListener(){
-			 
-            public void onClick(View v)
-            {
-                try{                    
-                         // CALL GetText method to make post method call
-                        GetText();
-                 }
-                catch(Exception ex)
-                 {
-                 
-                 }
-            }
-        }); 
+		result=(TextView)findViewById(R.id.result);
+		//String formhash="234a6898";
+		login.setOnClickListener(this);
+	
     }
 	
-	public  void  GetText()  throws  UnsupportedEncodingException
-    {
-        // Get user defined values
-       
-        stracc= login.getText().toString();
-        strpass= pass.getText().toString();
-         
-         // Create data variable for sent values to server 
-         
-          String data =  data += "&" + URLEncoder.encode("user", "UTF-8")
-                      + "=" + URLEncoder.encode(stracc, "UTF-8");
+public void postLoginData(){
+	HttpClient httpclient=new DefaultHttpClient();
+	
+	HttpPost httppost=new HttpPost("http://hkacgf.net/logging.php?action=login");
+	try{
+		String username=acc.getText().toString();
+		String password=pass.getText().toString();
+		
+		
+		List<NameValuePair> nameValuePairs=new ArrayList<NameValuePair>(2);
+		nameValuePairs.add(new BasicNameValuePair("username",username));
+		nameValuePairs.add(new BasicNameValuePair("password",password));
+		httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		Log.w("Login State","Execute HTTP Post Request");
+		HttpResponse response=httpclient.execute(httppost);
+		String str=inputStreamToString(response.getEntity().getContent()).toString();
+		Log.w("Login State", str);
+		if(str.toString().equalsIgnoreCase("true"))
+		{
+			Log.w("Lonin State", "TRUE");
+			result.setText("Login successful");
+		}else
+		{
+			Log.w("Login State","FALSE");
+			result.setText(str);
+		}
+	}catch(ClientProtocolException e){
+		e.printStackTrace();
+	}catch(IOException e){
+		e.printStackTrace();
+	}
+}
 
-          data += "&" + URLEncoder.encode("pass", "UTF-8")
-                      + "=" + URLEncoder.encode(strpass, "UTF-8");
-          
-          String text = "";
-          BufferedReader reader=null;
+private StringBuilder inputStreamToString(InputStream is){
+	String line="";
+	StringBuilder total=new StringBuilder();
+	BufferedReader rd=new BufferedReader(new InputStreamReader(is));
+	try{
+		while((line=rd.readLine()) !=null){
+			total.append(line);
+		}
+	}catch(IOException e){
+		e.printStackTrace();
+	}
+	return total;
+}
 
-          // Send data
-        try
-        {
-          
-            // Defined URL  where to send data
-            URL url = new URL("http://hkacgf.net/logging.php?action=login&");
-             
-         // Send POST data request
-
-          URLConnection conn = url.openConnection();
-          conn.setDoOutput(true);
-          OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-          wr.write( data );
-          wr.flush();
-      
-          // Get the server response
-           
-        reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        
-        // Read Server Response
-        while((line = reader.readLine()) != null)
-            {
-                   // Append server response in string
-                   sb.append(line + "\n");
-            }
-            
-            
-            text = sb.toString();
-        }
-        catch(Exception ex)
-        {
-             
-        }
-        finally
-        {
-            try
-            {
- 
-                reader.close();
-            }
-
-            catch(Exception ex) {}
-        }
-              
-        // Show response on activity
-       
-        
-    }
+public void onClick(View view){
+	if(view==login){
+		postLoginData();
+	}
+}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
